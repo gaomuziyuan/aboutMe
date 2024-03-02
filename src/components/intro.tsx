1f83df75c6a8de001f12f49119dd2e8641c26fbe
muziyuan <gaomuziyuan@icloud.com>
2024-03-02 14:31:16 -0800

    introdution

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
+import { FaArrowCircleRight, FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/app/context/active-section-context";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -30,8 +30,8 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              src="https://media.licdn.com/dms/image/C5603AQE-p6b4FJjt2Q/profile-displayphoto-shrink_800_800/0/1517725552797?e=1714608000&v=beta&t=0y61us1jvCHoJjQ7-3j_94jFHUdkV3p-GF0fyyBjJxg"
+              alt="Muziyuan portrait"
               width="192"
               height="192"
               quality="95"
@@ -61,11 +61,12 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Muziyuan.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">5 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. I am familiar
+        with{" "}
+        <span className="underline">React (Next.js) and Vue.js (Nuxt)</span>.
       </motion.h1>
 
       <motion.div
@@ -85,7 +86,7 @@ export default function Intro() {
           }}
         >
           Contact me here{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <FaArrowCircleRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
         <a
@@ -102,7 +103,7 @@ export default function Intro() {
           href="https://linkedin.com"
           target="_blank"
         >
-          <BsLinkedin />
+          <FaLinkedin />
         </a>
 
         <a
---
+++ src/lib/hooks.ts
@@ -0,0 +1,14 @@
+import { useActiveSectionContext } from "@/app/context/active-section-context";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+export function useSectionInView(sectionName: string, threshold = 0.75) {
+  const { ref, inView } = useInView({ threshold });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection(sectionName);
+    }
+  }, [inView, sectionName, setActiveSection, timeOfLastClick]);
+  return { ref };
+}
