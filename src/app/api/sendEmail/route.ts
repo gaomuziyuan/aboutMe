5e664ae8da63b2ee622ec4736f60c9905b81c7ec
muziyuan <gaomuziyuan@icloud.com>
2024-03-04 01:39:50 -0800

    change request type

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,35 +1,44 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function middleware(req: NextRequest) {}
+export async function POST(req: NextRequest, res: NextResponse) {
   const transporter = nodemailer.createTransport({
     // transporter configuration
   });
 
-  try {
-    const { senderEmail, message } = req.body;
+  if (req.method === "POST") {
+    try {
+      const body = await req.json();
+      const { senderEmail, message } = body;
 
-    const mailOptions = {
-      from: "muziyuangao@gmail.com",
-      to: senderEmail,
-      subject: "New Message from Contact Form",
-      text: message,
-    };
+      const mailOptions = {
+        from: "muziyuangao@example.com",
+        to: senderEmail,
+        subject: "New Message from Contact Form",
+        text: message,
+      };
 
-    const emailResponse = await transporter.sendMail(mailOptions);
+      const emailResponse = await transporter.sendMail(mailOptions);
 
-    console.log();
-
-    res.status(200).json({
-      success: true,
-      message: "Email sent successfully",
-      data: emailResponse,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Failed to send email",
-      error: (error as Error).message,
-    });
+      return new NextResponse(
+        JSON.stringify({
+          success: true,
+          message: "Email sent successfully",
+          data: emailResponse,
+        }),
+        { status: 200, headers: { "Content-Type": "application/json" } }
+      );
+    } catch (error) {
+      return new NextResponse(
+        JSON.stringify({
+          success: false,
+          message: "Failed to send email",
+          error: (error as Error).message,
+        }),
+        { status: 500, headers: { "Content-Type": "application/json" } }
+      );
+    }
+  } else {
+    return new NextResponse(null, { status: 405 });
   }
 }
